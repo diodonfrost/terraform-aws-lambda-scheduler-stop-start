5dc1ef1d318792ba69e36250c78108a4decced59
diodonfrost <diodon.frost@diodonfrost.me>
2019-02-02 17:41:53 +0100

    add terraform code

---
+++ main.tf
@@ -0,0 +1,153 @@
+################################################
+#
+#            IAM CONFIGURATION
+#
+################################################
+
+# Create role for stop and start aws resouces
+resource "aws_iam_role" "scheduler_lambda" {
+  name        = "scheduler_lambda_stop_start"
+  description = "Allows Lambda functions to stop and start ec2 and rds resources"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Create policy for manage autoscaling
+resource "aws_iam_policy" "schedule_autoscaling" {
+  name        = "autoscaling-custom-policy-start-stop"
+  description = "allow shutdown and startup autoscaling instances"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": [
+            "autoscaling:DescribeAutoScalingGroups",
+            "autoscaling:DescribeTags",
+            "autoscaling:UpdateAutoScalingGroup"
+        ],
+        "Resource": "*",
+        "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Create custom policy for manage ec2
+resource "aws_iam_policy" "schedule_ec2" {
+  name        = "ec2-custom-policy-start-stop"
+  description = "allow shutdown and startup ec2 instances"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": [
+            "ec2:DescribeInstances",
+            "ec2:DescribeInstanceStatus",
+            "ec2:StopInstances",
+            "ec2:StartInstances",
+            "ec2:DescribeTags"
+        ],
+        "Resource": "*",
+        "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Create custom policy for manage rds
+resource "aws_iam_policy" "schedule_rds" {
+  name        = "rds-custom-policy-start-stop"
+  description = "allow shutdown and startup rds instances"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "rds:ListTagsForResource",
+        "rds:DescribeDBClusters",
+        "rds:StartDBCluster",
+        "rds:StopDBCluster"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach custom policy autoscaling to role
+resource "aws_iam_role_policy_attachment" "autoscaling" {
+  role       = "${aws_iam_role.scheduler_lambda.name}"
+  policy_arn = "${aws_iam_policy.schedule_autoscaling.arn}"
+}
+
+# Attach custom policy ec2 to role
+resource "aws_iam_role_policy_attachment" "ec2" {
+  role       = "${aws_iam_role.scheduler_lambda.name}"
+  policy_arn = "${aws_iam_policy.schedule_ec2.arn}"
+}
+
+# Attach custom policy rds to role
+resource "aws_iam_role_policy_attachment" "rds" {
+  role       = "${aws_iam_role.scheduler_lambda.name}"
+  policy_arn = "${aws_iam_policy.schedule_rds.arn}"
+}
+
+
+################################################
+#
+#            LAMBDA FUNCTION
+#
+################################################
+
+
+# Convert *.py to .zip because AWS Lambda need .zip
+data "archive_file" "convert_py_to_zip" {
+  type        = "zip"
+  source_file = "${path.module}/package/aws_stop_start_resources.py"
+  output_path = "${path.module}/package/aws_stop_start_resources.py.zip"
+}
+
+# Create Lambda function for stop or start aws resources
+resource "aws_lambda_function" "stop_start" {
+  filename         = "${data.archive_file.convert_py_to_zip.output_path}"
+  function_name    = "aws-stop-start-resources"
+  role             = "${aws_iam_role.scheduler_lambda.arn}"
+  handler          = "aws_stop_start_resources.py.lambda_handler"
+  source_code_hash = "${data.archive_file.convert_py_to_zip.output_base64sha256}"
+  runtime          = "python3.7"
+  timeout          = "600"
+  environment {
+    variables = {
+      SCHEDULE_ACTION      = "${var.schedule_action}"
+      TAG_KEY              = "${var.resources_tag["key"]}"
+      TAG_VALUE            = "${var.resources_tag["value"]}"
+      EC2_SCHEDULE         = "${var.ec2_schedule}"
+      RDS_SCHEDULE         = "${var.rds_schedule}"
+      AUTOSCALING_SCHEDULE = "${var.autoscaling_schedule}"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+# Terraform variables file
+
+# Set cloudwatch events for shutingdown instances
+# Shutdown tous les vendredis
+# Shutdow à noel à 22h : ["cron(0 22 ? * FRI *)","cron(00 22 24 12 ? *)"]
+# Shutdown tous les soirs à 22h du lundi au vendredi
+# cf doc : https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
+variable "cloudwatch_event_schedule_expression" {
+  description = "Define the aws cloudwatch event rule schedule expression"
+  default = ["cron(0 22 ? * MON-FRI *)"]
+}
+
+variable "schedule_action" {
+  description = "Define schedule action to apply on resources, accepted value are 'stop or 'start"
+  default = "stop"
+}
+
+variable "resources_tag" {
+  type = "map"
+  description = "Set the tag use for identify resources to stop or start"
+  default = {
+    key   = "tostop"
+    value = "true"
+  }
+}
+
+variable "ec2_schedule" {
+  description = "Enable scheduling on ec2 resources"
+  default = true
+}
+
+variable "rds_schedule" {
+  description = "Enable scheduling on rds resources"
+  default = true
+}
+
+variable "autoscaling_schedule" {
+  description = "Enable scheduling on autoscaling group resources"
+  default = true
+}
+
+variable "autoscaling_start_params" {
+  description = "Define min_size instance to apply on autoscalingroup"
+  default = {
+    my_autoscalinggroup_name1 = "2"
+    my_autoscalinggroup_name2 = "3"
+    my_autoscalinggroup_name3 = "2"
+  }
+}
