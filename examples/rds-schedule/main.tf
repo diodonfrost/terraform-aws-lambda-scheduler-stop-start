b4edbeb5e3f4ede163a7cdb913ccd707d01b5de1
diodonfrost <diodon.frost@diodonfrost.me>
2019-06-19 22:42:38 +0200

    update terraform rds example

--- examples/rds-schedule/main.tf
+++ examples/rds-schedule/main.tf
@@ -1,16 +1,37 @@
 # Terraform rds with lambda scheduler
 
-# Get all availability zones
+# Get aws availability zones
 data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.103.0.0/16"
+}
+
+resource "aws_subnet" "primary" {
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "10.103.98.0/24"
+}
+
+resource "aws_subnet" "secondary" {
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "10.103.99.0/24"
+}
+
+resource "aws_db_subnet_group" "aurora" {
+  name       = "aurora-subnet"
+  subnet_ids = ["${aws_subnet.primary.id}", "${aws_subnet.secondary.id}"]
+}
 
 # Create rds aurora cluster
 resource "aws_rds_cluster" "aurora_with_tag" {
-  cluster_identifier  = "aurora-cluster-with-tag"
-  availability_zones  = ["data.aws_availability_zones.availability.names"]
-  database_name       = "aurorawithtag"
-  master_username     = "foo"
-  master_password     = "barbut8chars"
-  skip_final_snapshot = "true"
+  cluster_identifier   = "aurora-cluster-with-tag"
+  db_subnet_group_name = "${aws_db_subnet_group.aurora.id}"
+  database_name        = "aurorawithtag"
+  master_username      = "foo"
+  master_password      = "barbut8chars"
+  skip_final_snapshot  = "true"
 
   tags = {
     tostop = "true"
@@ -18,23 +39,25 @@ resource "aws_rds_cluster" "aurora_with_tag" {
 }
 
 resource "aws_rds_cluster_instance" "aurora_with_tag" {
-  identifier         = "aurora-cluster-with-tag"
-  cluster_identifier = "${aws_rds_cluster.aurora_with_tag.id}"
-  instance_class     = "db.t2.small"
+  identifier           = "aurora-cluster-with-tag"
+  db_subnet_group_name = "${aws_db_subnet_group.aurora.id}"
+  cluster_identifier   = "${aws_rds_cluster.aurora_with_tag.id}"
+  instance_class       = "db.t2.small"
 }
 
 # Create rds mariadb instance with tag
 resource "aws_db_instance" "mariadb_with_tag" {
-  identifier          = "mariadb-instance-with-tag"
-  name                = "mariadbwithtag"
-  allocated_storage   = 10
-  storage_type        = "gp2"
-  engine              = "mariadb"
-  engine_version      = "10.3"
-  instance_class      = "db.t2.micro"
-  username            = "foo"
-  password            = "foobarbaz"
-  skip_final_snapshot = "true"
+  identifier           = "mariadb-instance-with-tag"
+  name                 = "mariadbwithtag"
+  db_subnet_group_name = "${aws_db_subnet_group.aurora.id}"
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mariadb"
+  engine_version       = "10.3"
+  instance_class       = "db.t2.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  skip_final_snapshot  = "true"
 
   tags = {
     tostop = "true"
@@ -43,16 +66,17 @@ resource "aws_db_instance" "mariadb_with_tag" {
 
 # Create rds mysql instance with tag
 resource "aws_db_instance" "mysql_without_tag" {
-  identifier          = "mysql-instance-without-tag"
-  name                = "mysqlwithouttag"
-  allocated_storage   = 10
-  storage_type        = "gp2"
-  engine              = "mysql"
-  engine_version      = "5.6"
-  instance_class      = "db.t2.micro"
-  username            = "foo"
-  password            = "foobarbaz"
-  skip_final_snapshot = "true"
+  identifier           = "mysql-instance-without-tag"
+  name                 = "mysqlwithouttag"
+  db_subnet_group_name = "${aws_db_subnet_group.aurora.id}"
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.6"
+  instance_class       = "db.t2.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  skip_final_snapshot  = "true"
 }
 
 ### Terraform modules ###
