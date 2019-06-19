5178a7182c20aea5bb3ff37983dce8a53a93a785
diodonfrost <diodon.frost@diodonfrost.me>
2019-06-19 22:59:30 +0200

    linting python main.yml

--- package/main.py
+++ package/main.py
@@ -6,17 +6,17 @@ import autoscaling_handler
 import ec2_handler
 import rds_handler
 
-# Retrieve variables from Lmanda ENVIRONMENT
-schedule_action = os.getenv('SCHEDULE_ACTION', 'stop')
-tag_key = os.getenv('TAG_KEY', 'tostop')
-tag_value = os.getenv('TAG_VALUE', 'true')
-ec2_schedule = os.getenv('EC2_SCHEDULE', 'true')
-rds_schedule = os.getenv('RDS_SCHEDULE', 'true')
-autoscaling_schedule = os.getenv('AUTOSCALING_SCHEDULE', 'true')
-
 
 def lambda_handler(event, context):
     """ Main function entrypoint for lambda """
+
+    # Retrieve variables from aws lambda ENVIRONMENT
+    schedule_action = os.getenv('SCHEDULE_ACTION', 'stop')
+    tag_key = os.getenv('TAG_KEY', 'tostop')
+    tag_value = os.getenv('TAG_VALUE', 'true')
+    ec2_schedule = os.getenv('EC2_SCHEDULE', 'true')
+    rds_schedule = os.getenv('RDS_SCHEDULE', 'true')
+    autoscaling_schedule = os.getenv('AUTOSCALING_SCHEDULE', 'true')
 
     if autoscaling_schedule == 'true':
         autoscaling_handler.autoscaling_schedule(
