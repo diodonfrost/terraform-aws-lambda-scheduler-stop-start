29ddcd566e5aadeff668efc1e4699d24c99e9c67
diodonfrost <diodon.frost@diodonfrost.me>
2019-07-30 23:23:20 +0200

    add tox tests

---
+++ test/sanity/terraform_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install the Latest version of Terraform
+sudo pip install ansible
+sudo ansible-galaxy install diodonfrost.terraform && sudo ln -s ~/.ansible/roles/diodonfrost.terraform ~/.ansible/roles/ansible-role-terraform
+sudo ansible-pull -U https://github.com/diodonfrost/ansible-role-terraform tests/test.yml
+terraform -version
+terraform init
+
+# Test Terraform syntax
+terraform validate \
+  -var "region=${AWS_REGION}" \
+  -var "name=stop-aws-resources" \
+  -var "cloudwatch_schedule_expression=cron(0 22 ? * MON-FRI *)" \
+  -var "schedule_action=stop" \
+  -var "ec2_schedule=true" \
+  -var "rds_schedule=true" \
+  -var "autoscaling_schedule=true"
+
+# Test Terraform fixture example
+cd examples/test_fixture || exist
+terraform init
+terraform validate
+terraform -v
