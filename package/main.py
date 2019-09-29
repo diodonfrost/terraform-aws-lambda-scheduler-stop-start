484f9192a5de19ef3bb804bdb73b5c331a9d5faa
diodonfrost <diodon.frost@diodonfrost.me>
2019-09-29 16:22:39 +0200

    lint: use design pattern strategy
    Use design pattern strategy for the main function entrypoint

--- package/main.py
+++ package/main.py
@@ -25,34 +25,23 @@ def lambda_handler(event, context):
 
     Terminate spot instances (spot instance cannot be stopped by a user)
     """
+    _strategy = {}
     # Retrieve variables from aws lambda ENVIRONMENT
-    schedule_action = os.getenv("SCHEDULE_ACTION", "stop")
-    tag_key = os.getenv("TAG_KEY", "tostop")
-    tag_value = os.getenv("TAG_VALUE", "true")
-    autoscaling_schedule = os.getenv("AUTOSCALING_SCHEDULE", "true")
-    spot_schedule = os.getenv("SPOT_SCHEDULE", "false")
-    ec2_schedule = os.getenv("EC2_SCHEDULE", "true")
-    rds_schedule = os.getenv("RDS_SCHEDULE", "true")
+    schedule_action = os.getenv("SCHEDULE_ACTION")
+    tag_key = os.getenv("TAG_KEY")
+    tag_value = os.getenv("TAG_VALUE")
+    _strategy[AutoscalingScheduler] = os.getenv("AUTOSCALING_SCHEDULE")
+    _strategy[SpotScheduler] = os.getenv("SPOT_SCHEDULE")
+    _strategy[Ec2Scheduler] = os.getenv("EC2_SCHEDULE")
+    _strategy[RdsScheduler] = os.getenv("RDS_SCHEDULE")
 
-    asg_scheduler = AutoscalingScheduler()
-    spot_scheduler = SpotScheduler()
-    ec2_scheduler = Ec2Scheduler()
-    rds_scheduler = RdsScheduler()
-
-    if autoscaling_schedule == "true" and schedule_action == "stop":
-        asg_scheduler.stop(tag_key, tag_value)
-    elif autoscaling_schedule == "true" and schedule_action == "start":
-        asg_scheduler.start(tag_key, tag_value)
-
-    if spot_schedule == "terminate" and schedule_action == "stop":
-        spot_scheduler.terminate(tag_key, tag_value)
-
-    if ec2_schedule == "true" and schedule_action == "stop":
-        ec2_scheduler.stop(tag_key, tag_value)
-    elif ec2_schedule == "true" and schedule_action == "start":
-        ec2_scheduler.start(tag_key, tag_value)
-
-    if rds_schedule == "true" and schedule_action == "stop":
-        rds_scheduler.stop(tag_key, tag_value)
-    elif rds_schedule == "true" and schedule_action == "start":
-        rds_scheduler.start(tag_key, tag_value)
+    for key, value in _strategy.items():
+        if value == "true":
+            strategy = key()
+            if schedule_action == "stop":
+                strategy.stop(tag_key, tag_value)
+            elif schedule_action == "start":
+                strategy.start(tag_key, tag_value)
+        elif value == "terminate" and schedule_action == "stop":
+            strategy = key()
+            strategy.terminate(tag_key, tag_value)
