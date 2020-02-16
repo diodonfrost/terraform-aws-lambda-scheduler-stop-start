1d9baf0b1dec7d0aa1b49c7d3294e63dbb17a347
diodonfrost <diodon.frost@diodonfrost.me>
2020-02-16 15:13:13 +0100

    update terraform examples

--- examples/test_fixture/main.tf
+++ examples/test_fixture/main.tf
@@ -1,8 +1,14 @@
 # Deploy two lambda for testing with awspec
 
+resource "aws_kms_key" "scheduler" {
+  description             = "test kms option on scheduler module"
+  deletion_window_in_days = 7
+}
+
 module "aws-stop-friday" {
   source                         = "../.."
   name                           = "stop-aws"
+  kms_key_arn                    = aws_kms_key.scheduler.arn
   cloudwatch_schedule_expression = "cron(0 23 ? * FRI *)"
   schedule_action                = "stop"
   autoscaling_schedule           = "true"
