e709ea394f1f7e88f4771537557b693e92251e71
diodonfrost <diodon.frost@diodonfrost.me>
2020-09-21 18:32:16 +0200

    refacto(cloudwatch-scheduler): use resource groups tagging api to filter alarms
    Previously aws tags were retrieved using the API call describe_alarms
    and list_tags_for_resource on each alarm which made a lot of api calls.
    
    This commit change this by using a single one api call with the aws
    resource groups tagging api.

--- package/scheduler/cloudwatch_handler.py
+++ package/scheduler/cloudwatch_handler.py
@@ -2,13 +2,12 @@
 
 """Cloudwatch alarm action scheduler."""
 
-from typing import Iterator
-
 import boto3
 
 from botocore.exceptions import ClientError
 
 from .exceptions import cloudwatch_exception
+from .filter_resources_by_tags import FilterByTags
 
 
 class CloudWatchAlarmScheduler(object):
@@ -22,6 +21,7 @@ class CloudWatchAlarmScheduler(object):
             )
         else:
             self.cloudwatch = boto3.client("cloudwatch")
+        self.tag_api = FilterByTags(region_name=region_name)
 
     def stop(self, tag_key: str, tag_value: str) -> None:
         """Aws Cloudwatch alarm disable function.
@@ -33,7 +33,12 @@ class CloudWatchAlarmScheduler(object):
         :param str tag_value:
             Aws tag value to use for filter resources
         """
-        for alarm_name in self.filter_alarms(tag_key, tag_value):
+        format_tag = [{"Key": tag_key, "Values": [tag_value]}]
+
+        for alarm_arn in self.tag_api.get_resources(
+            "cloudwatch:alarm", format_tag
+        ):
+            alarm_name = alarm_arn.split(":")[-1]
             try:
                 self.cloudwatch.disable_alarm_actions(AlarmNames=[alarm_name])
                 print("Disable Cloudwatch alarm {0}".format(alarm_name))
@@ -50,42 +55,14 @@ class CloudWatchAlarmScheduler(object):
         :param str tag_value:
             Aws tag value to use for filter resources
         """
-        for alarm_name in self.filter_alarms(tag_key, tag_value):
+        format_tag = [{"Key": tag_key, "Values": [tag_value]}]
+
+        for alarm_arn in self.tag_api.get_resources(
+            "cloudwatch:alarm", format_tag
+        ):
+            alarm_name = alarm_arn.split(":")[-1]
             try:
                 self.cloudwatch.enable_alarm_actions(AlarmNames=[alarm_name])
                 print("Enable Cloudwatch alarm {0}".format(alarm_name))
             except ClientError as exc:
                 cloudwatch_exception("cloudwatch alarm", alarm_name, exc)
-
-    def filter_alarms(self, tag_key: str, tag_value: str) -> Iterator[str]:
-        """Aws Cloudwatch alarm filter function.
-
-        List name of all Cloudwatch alarm with the defined tag.
-
-        :param str tag_key:
-            Aws tag key to use for filter resources
-        :param str tag_value:
-            Aws tag value to use for filter resources
-
-        :yield Iterator[str]:
-            The Name of Cloudwatch alarm.
-        """
-        paginator = self.cloudwatch.get_paginator("describe_alarms")
-        alarm_arn_and_name_list = []
-        cloudwatch_tag_to_filter = {"Key": tag_key, "Value": tag_value}
-
-        for page in paginator.paginate():
-            for metric_alarm in page["MetricAlarms"]:
-                alarm_arn_and_name_list.append(
-                    {
-                        "arn": metric_alarm["AlarmArn"],
-                        "name": metric_alarm["AlarmName"],
-                    }
-                )
-
-        for alarm_arn_and_name in alarm_arn_and_name_list:
-            for alarm_tag in self.cloudwatch.list_tags_for_resource(
-                ResourceARN=alarm_arn_and_name["arn"]
-            )["Tags"]:
-                if cloudwatch_tag_to_filter == alarm_tag:
-                    yield alarm_arn_and_name["name"]
