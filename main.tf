cdf473740b6eaa605de2ee535951c48741a97bff
diodonfrost <diodon.frost@diodonfrost.me>
2021-01-01 00:14:00 +0100

    feat(iam): add aws tags on iam role resource
    This commit add "tags" parameter on terraform
    iam role resource.

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "aws_iam_role" "this" {
   name               = "${var.name}-scheduler-lambda"
   description        = "Allows Lambda functions to stop and start ec2 and rds resources"
   assume_role_policy = data.aws_iam_policy_document.this.json
+  tags               = var.tags
 }
 
 data "aws_iam_policy_document" "this" {
