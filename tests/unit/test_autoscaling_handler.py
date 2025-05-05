25eda0b355afeaf2a3d27f3c65797b5f8deebe23
diodonfrost <diodon.frost@diodonfrost.me>
2025-05-05 18:54:16 +0000

    refactor(unit-tests): use pytest's parametrize decorator
    Updated the unit tests for AutoscalingScheduler to leverage
    pytest's parametrize decorator, allowing each test to run with
    multiple AWS regions, tag sets, and expected EC2/ASG states.
    
    This change removes hardcoded values, increases test coverage,
    and makes it easier to add new scenarios by simply extending
    the parameter lists.

--- tests/unit/test_autoscaling_handler.py
+++ tests/unit/test_autoscaling_handler.py
@@ -1,7 +1,7 @@
 import boto3
 from moto import mock_aws
-import pytest
 from package.scheduler.autoscaling_handler import AutoscalingScheduler
+import pytest
 
 
 @mock_aws
@@ -19,12 +19,31 @@ def test_autoscaling_scheduler_initialization():
     assert scheduler.asg is not None
     assert scheduler.waiter is not None
 
-
+@pytest.mark.parametrize(
+    "aws_region, aws_tags, ec2_state, asg_state",
+    [
+        (
+            "eu-west-1",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "stopped", []
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "stopped", []
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "badtagkey", "Values": ["badtagvalue"]}],
+            "running", ["SuspendedProcesses"]
+        ),
+    ],
+)
 @mock_aws
-def test_stop_autoscaling_group():
+def test_stop_autoscaling_group(aws_region, aws_tags, ec2_state, asg_state):
     """Test stopping Auto Scaling groups and their instances."""
-    ec2 = boto3.client("ec2", region_name="us-east-1")
-    asg = boto3.client("autoscaling", region_name="us-east-1")
+    ec2 = boto3.client("ec2", region_name=aws_region)
+    asg = boto3.client("autoscaling", region_name=aws_region)
 
     asg.create_launch_configuration(
         LaunchConfigurationName="test-launch-config",
@@ -39,31 +58,50 @@ def test_stop_autoscaling_group():
         MinSize=1,
         MaxSize=1,
         DesiredCapacity=1,
-        AvailabilityZones=["us-east-1a"],
+        AvailabilityZones=[f"{aws_region}a"],
         Tags=[
-            {"Key": "Environment", "Value": "Development", "PropagateAtLaunch": True}
+            {"Key": "tostop", "Value": "true", "PropagateAtLaunch": True}
         ],
     )
 
     response = ec2.describe_instances()
     instance_id = response["Reservations"][0]["Instances"][0]["InstanceId"]
 
-    scheduler = AutoscalingScheduler(region_name="us-east-1")
-    aws_tags = [{"Key": "Environment", "Values": ["Development"]}]
+    scheduler = AutoscalingScheduler(region_name=aws_region)
     scheduler.stop(aws_tags)
 
     response = asg.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
-    assert response["AutoScalingGroups"][0]["SuspendedProcesses"] != []
+    assert response["AutoScalingGroups"][0]["SuspendedProcesses"] != asg_state
 
     response = ec2.describe_instances(InstanceIds=[instance_id])
-    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == "stopped"
+    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == ec2_state
 
 
+@pytest.mark.parametrize(
+    "aws_region, aws_tags, ec2_state, asg_state",
+    [
+        (
+            "eu-west-1",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "running", []
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "running", []
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "badtagkey", "Values": ["badtagvalue"]}],
+            "running", ["SuspendedProcesses"]
+        ),
+    ],
+)
 @mock_aws
-def test_start_autoscaling_group():
+def test_start_autoscaling_group(aws_region, aws_tags, ec2_state, asg_state):
     """Test starting Auto Scaling groups and their instances."""
-    ec2 = boto3.client("ec2", region_name="us-east-1")
-    asg = boto3.client("autoscaling", region_name="us-east-1")
+    ec2 = boto3.client("ec2", region_name=aws_region)
+    asg = boto3.client("autoscaling", region_name=aws_region)
 
     asg.create_launch_configuration(
         LaunchConfigurationName="test-launch-config",
@@ -78,9 +116,9 @@ def test_start_autoscaling_group():
         MinSize=1,
         MaxSize=1,
         DesiredCapacity=1,
-        AvailabilityZones=["us-east-1a"],
+        AvailabilityZones=[f"{aws_region}a"],
         Tags=[
-            {"Key": "Environment", "Value": "Development", "PropagateAtLaunch": True}
+            {"Key": "tostop", "Value": "true", "PropagateAtLaunch": True}
         ],
     )
 
@@ -90,22 +128,42 @@ def test_start_autoscaling_group():
     ec2.stop_instances(InstanceIds=[instance_id])
     asg.suspend_processes(AutoScalingGroupName=asg_name)
 
-    scheduler = AutoscalingScheduler(region_name="us-east-1")
-    aws_tags = [{"Key": "Environment", "Values": ["Development"]}]
+    scheduler = AutoscalingScheduler(region_name=aws_region)
+    aws_tags = [{"Key": "tostop", "Values": ["true"]}]
     scheduler.start(aws_tags)
 
     response = asg.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name])
     assert response["AutoScalingGroups"][0]["SuspendedProcesses"] == []
 
     response = ec2.describe_instances(InstanceIds=[instance_id])
-    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == "running"
+    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == ec2_state
 
 
+@pytest.mark.parametrize(
+    "aws_region, aws_tags, ec2_state",
+    [
+        (
+            "eu-west-1",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "terminated"
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "tostop", "Values": ["true"]}],
+            "terminated"
+        ),
+        (
+            "eu-west-2",
+            [{"Key": "badtagkey", "Values": ["badtagvalue"]}],
+            "running"
+        ),
+    ],
+)
 @mock_aws
-def test_terminate_instances():
+def test_terminate_instances(aws_region, aws_tags, ec2_state):
     """Test terminating instances in Auto Scaling groups."""
-    ec2 = boto3.client("ec2", region_name="us-east-1")
-    asg = boto3.client("autoscaling", region_name="us-east-1")
+    ec2 = boto3.client("ec2", region_name=aws_region)
+    asg = boto3.client("autoscaling", region_name=aws_region)
 
     asg.create_launch_configuration(
         LaunchConfigurationName="test-launch-config",
@@ -120,21 +178,20 @@ def test_terminate_instances():
         MinSize=1,
         MaxSize=1,
         DesiredCapacity=1,
-        AvailabilityZones=["us-east-1a"],
+        AvailabilityZones=[f"{aws_region}a"],
         Tags=[
-            {"Key": "Environment", "Value": "Development", "PropagateAtLaunch": True}
+            {"Key": "tostop", "Value": "true", "PropagateAtLaunch": True}
         ],
     )
 
     response = ec2.describe_instances()
     instance_id = response["Reservations"][0]["Instances"][0]["InstanceId"]
 
-    scheduler = AutoscalingScheduler(region_name="us-east-1")
-    aws_tags = [{"Key": "Environment", "Values": ["Development"]}]
+    scheduler = AutoscalingScheduler(region_name=aws_region)
     scheduler.stop(aws_tags, terminate_instances=True)
 
     response = ec2.describe_instances(InstanceIds=[instance_id])
-    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == "terminated"
+    assert response["Reservations"][0]["Instances"][0]["State"]["Name"] == ec2_state
 
 
 @mock_aws
