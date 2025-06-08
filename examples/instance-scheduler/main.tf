25656f5781983a41369ea3337048aee3640cc5e5
diodonfrost <diodon.frost@diodonfrost.me>
2025-06-08 21:47:35 +0000

    tests(examples): use ubuntu 24.04 for autoscaling and instance scheduler examples
    Ubuntu 18.04 (Bionic) has reached end-of-life and is no longer
    available in AWS AMI catalog. Updated all example configurations
    to use Ubuntu 24.04 (Noble) LTS for continued security support
    and compatibility.

--- examples/instance-scheduler/main.tf
+++ examples/instance-scheduler/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
